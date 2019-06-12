8bfe6308fa76bdd9bd8a5d5e5f950542b05964a5
floppycamel22 <floppycamel1022@gmail.com>
2019-06-12 07:40:16 +0000

    Relationship model add

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,28 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attachment :profile_image
 
   validates :user_name, presence: true, length: {  maximum: 20 }
   validates :email, presence: true, length: { in: 3..25 }
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationship.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
